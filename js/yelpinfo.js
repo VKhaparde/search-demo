6c91b8ed9ee0b07746e11699115d98d20f7fb303
Vandana Khaparde <vandana.khaparde@gmail.com>
2020-04-09 14:44:33 +0000

    Can display search results on the screen

--- js/yelpinfo.js
+++ js/yelpinfo.js
@@ -23,15 +23,21 @@ class YelpInfo{
   }
   getSearchResults(userData){
     let ajaxConfigObj = {
+      async: true,
+      crossDomain: true,
       dataType : "json",
-      url: 'yelpproxy.php',
+      url: "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search",
+      // url : "yelpproxy.php",
       data: {
         term : userData.userSearchTopic,
-        location:userData.userSearchAddress,
-        radius:userData.userSearchRadius
+        location:userData.userSearchAddress
+        // radius:userData.userSearchRadius
       },
      method:'get',
       "headers": {
+        "accept": "application/json",
+        "x-requested-with": "xmlhttprequest",
+        "Access-Control-Allow-Origin": "*",
         "Authorization": "Bearer Tk6PRF-9Y5t64vllCs1WMt9TKTajUyDxc31SMcG2a6ENbj5ZI4bjyYzAPcefYWk9MoNKO9aBJdek0exD3wjVfXDb0G8xQHmiB541ekuYs6UzZpfXXvc3qFu9CLa3XXYx",
       },
       success: response => {
@@ -41,6 +47,24 @@ class YelpInfo{
         alert("Sorry! Cannot get the results at this time.")
       }
     };
-    $ajax(ajaxConfigObj);
+    $.ajax(ajaxConfigObj);
+  }
+  displayUserResults(responseData){
+    console.log(responseData);
+    $(".businesses").empty();
+    responseData.businesses.map((currentVal)=>{
+      this.domElements.name  = $('<div>').addClass("name").text(currentVal.name);
+      this.domElements.phoneNumber = $('<div>').addClass("phone").text(currentVal.display_phone);
+      this.domElements.rating = $('<div>').addClass("rating").text(currentVal.rating)
+      this.domElements.address = $('<div>').addClass("address");
+      //.text(currentVal.location.display_address[0])
+      let addressPart = currentVal.location.display_address;
+      addressPart.map((element)=>{
+         let addressLine = $('<div>').addClass('addressLine').text(element);
+        this.domElements.address.append(addressLine);
+      });
+      $(".businesses").append(this.domElements.name,this.domElements.phoneNumber,
+        this.domElements.rating,this.domElements.address);
+    })
   }
 }
