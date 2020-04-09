f578c37a76574a651fe6812e0c58474dbcdafa88
Vandana Khaparde <vandana.khaparde@gmail.com>
2020-04-09 18:18:08 +0000

    display message for no results

--- js/yelpinfo.js
+++ js/yelpinfo.js
@@ -9,13 +9,10 @@ class YelpInfo{
   };
 }
   getData = ()=>{
-    // let userSearchTopic = $('.searchTopic').val();
-    // let userSearchAddress = $('.searchAddress').val();
-    // let userSearchRadius = $('.searchRadius').val();
     let userData = {
       userSearchTopic : $('.searchTopic').val(),
       userSearchAddress : $('.searchAddress').val(),
-      userSearchRadius : $('.searchRadius').val()
+      userSearchRadius: (parseInt(($('.searchRadius').val()) * 1609.34)).toString()
     }
     console.log(userData.userSearchTopic,userData.userSearchAddress,userData.userSearchRadius);
     console.log(userData);
@@ -27,11 +24,10 @@ class YelpInfo{
       crossDomain: true,
       dataType : "json",
       url: "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search",
-      // url : "yelpproxy.php",
       data: {
         term : userData.userSearchTopic,
-        location:userData.userSearchAddress
-        // radius:userData.userSearchRadius
+        location:userData.userSearchAddress,
+        radius:userData.userSearchRadius
       },
      method:'get',
       "headers": {
@@ -52,21 +48,26 @@ class YelpInfo{
   displayUserResults(responseData){
     console.log(responseData);
     $(".businesses").empty();
-    responseData.businesses.map((currentVal)=>{
-      this.domElements.name  = $('<div>').addClass("name").text(currentVal.name);
-      this.domElements.phoneNumber = $('<div>').addClass("phone").text(currentVal.display_phone);
-      this.domElements.rating = $('<div>').addClass("rating").text(currentVal.rating)
-      this.domElements.address = $('<div>').addClass("address");
-      //.text(currentVal.location.display_address[0])
-      let addressPart = currentVal.location.display_address;
-      addressPart.map((element)=>{
-         let addressLine = $('<div>').addClass('addressLine').text(element);
-        this.domElements.address.append(addressLine);
-      });
-      var businessElement = $('<div>').addClass('businessElement');
-      businessElement.append(this.domElements.name,this.domElements.phoneNumber,
-        this.domElements.rating,this.domElements.address);
-      $(".businesses").append(businessElement);
-    })
+    if(responseData.businesses.length === 0){
+      $(".businesses").addClass("noData").text("Sorry! No data is available");
+    }
+    else{
+      responseData.businesses.map((currentVal) => {
+        this.domElements.name = $('<div>').addClass("name").text(currentVal.name);
+        this.domElements.phoneNumber = $('<div>').addClass("phone").text(currentVal.display_phone);
+        this.domElements.rating = $('<div>').addClass("rating").text(currentVal.rating)
+        this.domElements.address = $('<div>').addClass("address");
+        //.text(currentVal.location.display_address[0])
+        let addressPart = currentVal.location.display_address;
+        addressPart.map((element) => {
+          let addressLine = $('<div>').addClass('addressLine').text(element);
+          this.domElements.address.append(addressLine);
+        });
+        var businessElement = $('<div>').addClass('businessElement');
+        businessElement.append(this.domElements.name, this.domElements.phoneNumber,
+          this.domElements.rating, this.domElements.address);
+        $(".businesses").append(businessElement);
+      })
+    }
   }
 }
