3aa79f3cffd480d1b691344a907bf28dba17044c
Vandana Khaparde <vandana.khaparde@gmail.com>
2020-04-09 00:06:47 +0000

    Making ajax call for yelp API

--- js/yelpinfo.js
+++ js/yelpinfo.js
@@ -9,10 +9,38 @@ class YelpInfo{
   };
 }
   getData = ()=>{
-    let userSearchTopic = $('.searchTopic').val();
-    let userSearchAddress = $('.searchAddress').val();
-    let userSearchRadius = $('.searchRadius').val();
-    console.log(userSearchTopic,userSearchAddress,userSearchRadius);
+    // let userSearchTopic = $('.searchTopic').val();
+    // let userSearchAddress = $('.searchAddress').val();
+    // let userSearchRadius = $('.searchRadius').val();
+    let userData = {
+      userSearchTopic : $('.searchTopic').val(),
+      userSearchAddress : $('.searchAddress').val(),
+      userSearchRadius : $('.searchRadius').val()
+    }
+    console.log(userData.userSearchTopic,userData.userSearchAddress,userData.userSearchRadius);
+    console.log(userData);
+    this.getSearchResults(userData);
   }
-
+  getSearchResults(userData){
+    let ajaxConfigObj = {
+      dataType : "json",
+      url: 'yelpproxy.php',
+      data: {
+        term : userData.userSearchTopic,
+        location:userData.userSearchAddress,
+        radius:userData.userSearchRadius
+      },
+     method:'get',
+      "headers": {
+        "Authorization": "Bearer Tk6PRF-9Y5t64vllCs1WMt9TKTajUyDxc31SMcG2a6ENbj5ZI4bjyYzAPcefYWk9MoNKO9aBJdek0exD3wjVfXDb0G8xQHmiB541ekuYs6UzZpfXXvc3qFu9CLa3XXYx",
+      },
+      success: response => {
+        this.displayUserResults(response);
+      },
+      error : ()=>{
+        alert("Sorry! Cannot get the results at this time.")
+      }
+    };
+    $ajax(ajaxConfigObj);
+  }
 }
